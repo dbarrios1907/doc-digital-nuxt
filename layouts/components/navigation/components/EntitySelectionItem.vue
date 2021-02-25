0857106d45d54b994e829ea6b1dcb59d61e3db35
Daniel Barrios <dbarrios1907@gmail.com>
2021-02-25 04:48:07 -0300

    feat(Navigation Entity Selection): Fix truncate values on Entity selection component

--- layouts/components/navigation/components/EntitySelectionItem.vue
+++ layouts/components/navigation/components/EntitySelectionItem.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="d-flex justify-center align-center px-4 py-2">
-    <span class="light--text" style="flex-grow: 0; flex-shrink: 0; text-overflow: ellipsis; white-space: nowrap; max-width: 260px">
-      Ministerio Secretaria General de...
-    </span>
+  <div class="d-flex justify-center align-center px-4 py-2" style="flex-shrink: 0">
+    <span class="d-flex-inline text-truncate light--text" style="max-width: 260px"> Ministerio Secretaria General de la Región Metropolitana </span>
     <CompareArrowsIcon class="ml-2" />
   </div>
 </template>
