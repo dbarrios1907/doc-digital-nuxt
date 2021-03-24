1ddf7d901697f2574bfa5150e3649cd3bfd72979
Daniel Barrios <dbarrios1907@gmail.com>
2021-03-24 10:18:48 -0300

    feat(in progress): Fix authentication and error handling issues

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // '~/modules/auth',
+    '~/modules/auth',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     'nuxt-webfontloader',
