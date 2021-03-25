b5c884dcecf0f1e3ebba98ed6b77a45b7792f3ea
Leonardo Enrique Reyna Castro <leonardo.reyna@dox.cl>
2021-03-24 23:59:04 -0300

    fix(inbox_filter): solucionado reset de campos del formulario de filtro para bandejas

--- components/doc-digital/modal/DocumentFilters/index.vue
+++ components/doc-digital/modal/DocumentFilters/index.vue
@@ -19,70 +19,72 @@
       </v-card-title>
       <v-divider class="darken1" />
       <perfect-scrollbar :style="{ maxHeight: _isSmall ? 'calc(100vh - 70px)' : '500px' }">
-        <v-card-text class="dx-card-content flex-fill font-roboto weight-400 line-height-30 font-title darken3--text pt-10 px-10 pb-3">
-          <div class="font-title line-height-30 mb-10">
-            Complete <b>el o los campos necesarios</b> para realizar la búsqueda del documento que desea.
-          </div>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-card-text class="dx-card-content flex-fill font-roboto weight-400 line-height-30 font-title darken3--text pt-10 px-10 pb-3">
+            <div class="font-title line-height-30 mb-10">
+              Complete <b>el o los campos necesarios</b> para realizar la búsqueda del documento que desea.
+            </div>
 
-          <v-row class="mt-3">
-            <v-col cols="12" sm="12" md="4" lg="4" class="py-sm-0 px-xs-0">
-              <span>Tema o materia:</span>
-              <dx-text-field v-model="d_tema" flat solo outlined placeholder="Escriba el tema/materia" />
-            </v-col>
-            <v-col cols="12" sm="12" md="4" lg="4" class="py-sm-0 px-xs-0">
-              <span>Tipo: </span>
-              <dx-select v-model="d_tipo" :items="documentOptions" label="Selecciona el tipo" />
-            </v-col>
-            <v-col cols="12" sm="12" md="4" lg="4" class="py-sm-0 px-xs-0">
-              <span>Folio:</span>
-              <dx-text-field v-model="d_folio" flat solo outlined placeholder="Escriba el folio" />
-            </v-col>
-          </v-row>
+            <v-row class="mt-3">
+              <v-col cols="12" sm="12" md="4" lg="4" class="py-sm-0 px-xs-0">
+                <span>Tema o materia:</span>
+                <dx-text-field v-model="d_tema" flat solo outlined placeholder="Escriba el tema/materia" />
+              </v-col>
+              <v-col cols="12" sm="12" md="4" lg="4" class="py-sm-0 px-xs-0">
+                <span>Tipo: </span>
+                <dx-select v-model="d_tipo" :items="documentOptions" label="Selecciona el tipo" />
+              </v-col>
+              <v-col cols="12" sm="12" md="4" lg="4" class="py-sm-0 px-xs-0">
+                <span>Folio:</span>
+                <dx-text-field v-model="d_folio" flat solo outlined placeholder="Escriba el folio" />
+              </v-col>
+            </v-row>
 
-          <v-row class="mt-6" no-gutters>
-            <v-col cols="12">
-              <div class="weight-700 darken3--text font-regular mb-3">Creación</div>
-            </v-col>
-            <v-col>
-              <div class="d-inline-block">Desde:</div>
-              <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
-                <dx-date-picker v-model="d_createdStart" no-title color="primary" cleared />
-              </div>
-            </v-col>
-            <v-col>
-              <div class="d-inline-block">Hasta:</div>
-              <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
-                <dx-date-picker v-model="d_createdEnd" no-title color="primary" cleared />
-              </div>
-            </v-col>
-          </v-row>
+            <v-row class="mt-6" no-gutters>
+              <v-col cols="12">
+                <div class="weight-700 darken3--text font-regular mb-3">Creación</div>
+              </v-col>
+              <v-col>
+                <div class="d-inline-block">Desde:</div>
+                <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
+                  <dx-date-picker v-model="d_createdStart" no-title color="primary" cleared />
+                </div>
+              </v-col>
+              <v-col>
+                <div class="d-inline-block">Hasta:</div>
+                <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
+                  <dx-date-picker v-model="d_createdEnd" no-title color="primary" cleared />
+                </div>
+              </v-col>
+            </v-row>
 
-          <v-row class="mt-0 mt-md-8" no-gutters>
-            <v-col cols="12">
-              <div class="weight-700 darken3--text font-regular mb-3">Actualización</div>
-            </v-col>
-            <v-col>
-              <div class="d-inline-block">Desde:</div>
-              <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
-                <dx-date-picker v-model="d_updatedStart" no-title color="primary" cleared />
-              </div>
-            </v-col>
-            <v-col>
-              <div class="d-inline-block">Hasta:</div>
-              <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
-                <dx-date-picker v-model="d_updatedEnd" no-title color="primary" cleared />
-              </div>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions class="px-10 pb-10 mt-2 d-flex justify-end justify-xs-center">
-          <dx-button color="primary" outlined class="text-none" @click="cancel">
-            <span class="text-underline"> Cancelar </span>
-          </dx-button>
-          <dx-button color="primary" class="text-none ml-5" @click="filter">
-            <span class="text-underline"> Buscar </span>
-          </dx-button>
-        </v-card-actions>
+            <v-row class="mt-0 mt-md-8" no-gutters>
+              <v-col cols="12">
+                <div class="weight-700 darken3--text font-regular mb-3">Actualización</div>
+              </v-col>
+              <v-col>
+                <div class="d-inline-block">Desde:</div>
+                <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
+                  <dx-date-picker v-model="d_updatedStart" no-title color="primary" cleared />
+                </div>
+              </v-col>
+              <v-col>
+                <div class="d-inline-block">Hasta:</div>
+                <div class="d-inline-block px-0 datepicker col-12 col-md-4 px-md-3">
+                  <dx-date-picker v-model="d_updatedEnd" no-title color="primary" cleared />
+                </div>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions class="px-10 pb-10 mt-2 d-flex justify-end justify-xs-center">
+            <dx-button color="primary" outlined class="text-none" @click="cancel">
+              <span class="text-underline"> Cancelar </span>
+            </dx-button>
+            <dx-button color="primary" class="text-none ml-5" @click="filter">
+              <span class="text-underline"> Buscar </span>
+            </dx-button>
+          </v-card-actions>
+        </v-form>
       </perfect-scrollbar>
     </v-card>
   </v-dialog>
@@ -96,6 +98,14 @@ export default {
   name: 'ModalDocumentFilters',
   mixins: [responsiveMixin, closeableMixin],
   inheritAttrs: false,
+  watch: {
+    iscancel: {
+      handler: function (after, before) {
+        this.cancel()
+      },
+      deep: true,
+    },
+  },
   props: {
     tema: String,
     tipo: String,
@@ -105,9 +115,12 @@ export default {
     updatedStart: String,
     updatedEnd: String,
     documentOptions: Array,
+    iscancel: Boolean,
   },
   data() {
     return {
+      valid: true,
+      iscancel: false,
       d_tema: this.tema,
       d_tipo: this.tipo,
       d_folio: this.folio,
@@ -119,14 +132,8 @@ export default {
   },
   methods: {
     cancel() {
-      this.d_tema = undefined
-      this.d_tipo = undefined
-      this.d_folio = undefined
-      this.d_createdStart = undefined
-      this.d_createdEnd = undefined
-      this.d_updatedStart = undefined
-      this.d_updatedEnd = undefined
       this.$emit('onCancel')
+      this.$refs.form.reset()
     },
     filter() {
       const { d_tema, d_tipo, d_folio, d_createdStart, d_createdEnd, d_updatedStart, d_updatedEnd } = this
